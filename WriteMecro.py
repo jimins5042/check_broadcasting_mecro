4df8d8935b3ceb5963f34c08859003c2ab847a7d
timetable5678 <coolcats6762@gmail.com>
2024-02-02 03:12:21 +0900

    -셀레니움 메소드 수정5

--- WriteMecro.py
+++ WriteMecro.py
@@ -11,7 +11,8 @@ class WriteMecro:
         print("start mecro")
         service = Service(executable_path=r'/usr/bin/chromedriver')
         options = webdriver.ChromeOptions()
-        options.add_argument('window-size=1920x1080')
+        #options.add_argument('window-size=1920x1080')
+        options.binary_location = "/snap/chromium/2738/usr/lib/chromium-browser/chrome"  # 실제 경로로 변경하세요
 
         options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
