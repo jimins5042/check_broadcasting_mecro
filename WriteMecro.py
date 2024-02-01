1f3eb13f58f41d58085916a6125acaa36f1f69ec
timetable5678 <coolcats6762@gmail.com>
2024-02-02 03:05:42 +0900

    -셀레니움 메소드 수정3

--- WriteMecro.py
+++ WriteMecro.py
@@ -1,7 +1,7 @@
 import time
 
 from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
