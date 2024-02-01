9d6fdf055af022dd12fb040d84a22d212072f061
timetable5678 <coolcats6762@gmail.com>
2024-02-02 00:00:08 +0900

    Initial commit

---
+++ WriteMecro.py
@@ -0,0 +1,108 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+class WriteMecro:
+
+    def dc_mecro(self, title):
+        print("start mecro")
+        service = Service(executable_path=r'C:/Windows/chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        options.add_argument('window-size=1920x1080')
+
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+        driver = webdriver.Chrome(service=service, options=options)
+
+        driver.get("https://gall.dcinside.com/mini/board/write/?id=iana")  # 갤러리 글쓰기 주소
+
+        time.sleep(2)
+
+        driver.find_element(By.ID, "btn_gall_nick_name_x").click()
+
+        driver.implicitly_wait(1)
+        driver.find_element(By.NAME, "name").send_keys(u'알림 테스트')  # 닉네임
+
+        driver.implicitly_wait(1)
+
+        time.sleep(2)
+
+        driver.find_element(By.NAME, "password").send_keys(u'12345')  # 비밀번호
+
+        driver.implicitly_wait(1)
+
+        time.sleep(1)
+
+        driver.find_element(By.XPATH, "//*[@id='write']/div[1]/fieldset/div[3]/ul/li[1]").click()
+
+        time.sleep(1)
+
+        driver.find_element(By.NAME, "subject").send_keys(u'test')  # 제목
+
+        driver.implicitly_wait(1)
+
+        time.sleep(1)
+
+        driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@name='tx_canvas_wysiwyg']"))
+
+        time.sleep(1)
+
+        driver.find_element(By.TAG_NAME, "body").send_keys(u"이아나 방송 알림봇 \n방송제목 : " + title)
+
+        time.sleep(1)
+
+        # 글등록
+        driver.switch_to.default_content()
+
+        time.sleep(5)
+
+        driver.find_element(By.CSS_SELECTOR, 'button.btn_lightpurple.btn_svc.write').click()
+        driver.implicitly_wait(1)
+
+        time.sleep(1)
+
+        driver.close()
+
+    def ruri_mecro(self):
+        service = Service(executable_path=r'C:/Windows/chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        options.add_argument('window-size=1920x1080')
+
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+        driver = webdriver.Chrome(service=service, options=options)
+
+        driver.get("https://user.ruliweb.com/member/login")  # 갤러리 목록 주소
+
+        driver.implicitly_wait(3)
+
+        time.sleep(3)
+
+        # time.sleep(2)
+
+        driver.find_element(By.ID, "user_id").send_keys(u'')  # 비밀번호
+
+        driver.implicitly_wait(1)
+
+        driver.find_element(By.ID, "user_pw").send_keys(u'')  # 비밀번호
+
+        driver.implicitly_wait(1)
+
+        driver.find_element(By.ID, 'login_submit').click()
+        time.sleep(3)
+
+        driver.get("https://bbs.ruliweb.com/community/board/301000/write")  # 갤러리 목록 주소
+        time.sleep(3)
+
+        driver.find_element(By.ID, "subject").send_keys(u'test')  # 비밀번호
+
+        driver.implicitly_wait(1)
+
+        driver.switch_to.frame(driver.find_element(By.XPATH, "//*[@id='editor_v2']/div/div[3]/div/div[2]/div[3]/div[2]"))
+        driver.find_element(By.ID, "editor_v2").send_keys(u'test body')  # 비밀번호
+        time.sleep(30)
---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, jsonify, request
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+import json
+import time
+
+from WriteMecro import WriteMecro
+
+# 3분마다 방송 상태 확인
+def scheduler():
+
+    check_Broadcasting()
+
+# 스케줄러 설정
+schedule = BackgroundScheduler(daemon=True, timezone='Asia/Seoul')
+schedule.add_job(scheduler, 'interval', seconds=5)
+schedule.start()
+
+
+app = Flask(__name__)
+
+# 전역 변수로 방송 상태를 저장
+streaming = False
+
+# 방송 상태 변경 메소드
+def streaming_task(value):
+    global streaming
+    streaming = value
+
+# 테스트용 ========
+@app.route('/dc')
+def write_dc():
+    dc = WriteMecro()
+    dc.dc_mecro()
+    return "Hello, World!"
+
+
+@app.route('/ruri')
+def write_ruri():
+    ruri = WriteMecro()
+    ruri.ruri_mecro()
+    return "Hello, World!"
+
+# ========
+
+@app.route('/')
+def start():
+    check_Broadcasting()
+
+# 핵심 비지니스 로직
+def check_Broadcasting():
+
+    # api를 통한 방송 정보 획득
+    res = requests.get('https://bjapi.afreecatv.com/api/ianavlup/station',
+                       headers={'User-Agent': 'Mozilla/5.0'}).json()
+
+    data = json.dumps(res)
+    data = json.loads(data)
+
+    # broad == 방송 정보(user_id, broad_no, broad_title, current_sum_viwer, broad_grade, is_passward
+    # 방송 없으면 null 값
+    board_values = data.get("broad")
+
+    print("방송 상태 체크")
+
+    # 방송을 하고 있고, 방송 정보가 false라면 글 작성 메크로 작동
+    if board_values != None:
+
+        if streaming == False:
+            # 방송 상태 변수값 변경
+            streaming_task(True)
+            print("방송중!!")
+
+            print(board_values)
+            title = board_values.get("broad_title")
+            print(title)
+
+            dc = WriteMecro()
+            # dc.dc_mecro(title)
+
+    # 방송 없을 경우
+    else:
+        if streaming == True:
+            # 방송 상태 변수값 변경
+            streaming_task(False)
+
+        print("No Broadcasting")
+
+    return board_values
+
+
+if __name__ == '__main__':
+    app.run()
