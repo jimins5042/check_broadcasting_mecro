b228d1fe4375ee7f49a9ceafede88e898e6ffe85
timetable5678 <coolcats6762@gmail.com>
2024-02-02 16:21:00 +0900

    -셀레니움 메소드 수정11

--- WriteMecro.py
+++ WriteMecro.py
@@ -1,10 +1,10 @@
 import time
 
-import ChromeDriverManager as ChromeDriverManager
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
+from webdriver_manager.chrome import ChromeDriverManager
 
 class WriteMecro:
 
